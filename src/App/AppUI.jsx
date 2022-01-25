2ca378c935d3ece5691c760aa6be4008da4570a6
Genaro Melgar <gjmg1612@gmail.com>
2022-01-25 14:59:27 -0600

    Toques finales

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -29,20 +29,21 @@ function AppUI() {
 
         { loading &&
           <ContentLoader
-            speed={2}
+            speed={4}
             width={400}
             height={160}
             viewBox="0 0 400 160"
             backgroundColor="#f3f3f3"
             foregroundColor="#ecebeb"
           >
+            <br /><br /><br />
+            <circle cx="20" cy="20" r="20" />
             <rect x="48" y="8" rx="3" ry="3" width="88" height="6" />
             <rect x="48" y="26" rx="3" ry="3" width="52" height="6" />
             <rect x="-2" y="53" rx="3" ry="3" width="410" height="6" />
             <rect x="0" y="72" rx="3" ry="3" width="380" height="6" />
             <rect x="0" y="88" rx="3" ry="3" width="178" height="6" />
-            <circle cx="20" cy="20" r="20" />
-          </ContentLoader> 
+          </ContentLoader>
       }
 
         {(!loading && !searchedTodos.length) && <p>Crea tu primer todo</p> }
